307971743b42de4eac66a351c5b34fbcfb219de1
Cho Seonghan <chosnhn1@gmail.com>
2025-09-01 16:49:23 +0900

    add cart templates & chores

--- shop/views.py
+++ shop/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import get_object_or_404, render
+
+from cart.forms import CartAddProductForm
 from .models import Category, Product
 
 # product catalogue view
@@ -16,4 +18,5 @@ def product_list(request, category_slug=None):
 # detailed single available product view
 def product_detail(request, id, slug):
     product = get_object_or_404(Product, id=id, slug=slug, available=True)
-    return render(request, 'shop/product/detail.html', {'product': product})
+    cart_product_form = CartAddProductForm()
+    return render(request, 'shop/product/detail.html', {'product': product, 'cart_product_form': cart_product_form})
